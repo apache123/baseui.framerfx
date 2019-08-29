3419d000c85ef0e3f025eecd0c661f950ec19389
iKettles <iain@framer.com>
2019-08-29 13:14:40 +0200

    Update managed state property when Pagination CurrentPage property control changes

--- code/navigation/Pagination.tsx
+++ code/navigation/Pagination.tsx
@@ -8,6 +8,7 @@ import { withHOC } from "../withHOC"
 const InnerPagination: React.SFC<any> = ({ currentPage, ...props }) => {
   const [page, setCurrentPage] = useManagedState(currentPage)
   const onPageChanged = React.useCallback(({ nextPage }) => setCurrentPage(Math.min(Math.max(nextPage, 1), 20)), [])
+  React.useEffect(() => setCurrentPage(currentPage), [currentPage])
 
   return <System.Pagination {...props} currentPage={page} onPageChange={onPageChanged} />
 }
