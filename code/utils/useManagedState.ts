b0fa845737be0e19569a5336679573ec3a9ce3d6
Fernando Hurtado <fernandohur@gmail.com>
2019-09-03 14:57:40 +0200

    re-add useEffect call to useManagedState

--- code/utils/useManagedState.ts
+++ code/utils/useManagedState.ts
@@ -3,5 +3,9 @@ import { useState, useCallback, useEffect } from "react"
 export function useManagedState<T>(initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [currentValue, setValue] = useState(initialValue)
 
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
   return [currentValue, setValue]
 }
