f3b9bf579b4590e491c225c3c412a3c839278f2f
iKettles <iain@framer.com>
2019-09-12 11:46:35 +0200

    Fixed Datepicker formatting and enabled range option

--- code/pickers/DatePicker.tsx
+++ code/pickers/DatePicker.tsx
@@ -1,18 +1,35 @@
 import * as System from "baseui/datepicker"
-import { addPropertyControls } from "framer"
+import { addPropertyControls, ControlType } from "framer"
 import * as React from "react"
 import { controls, merge } from "../generated/Datepicker"
 import { useManagedState } from "../utils/useManagedState"
 import { withHOC } from "../withHOC"
 
-const InnerDatepicker: React.SFC<any> = ({ date, ...props }) => {
-  const [value, setValue] = useManagedState(date)
-  return <System.Datepicker {...props} value={value} onChange={e => setValue(e.date)} />
+function parseDateString(date) {
+  const parsedDate = new Date(date)
+  if (isNaN(parsedDate.valueOf())) {
+    return null
+  }
+  return parsedDate
+}
+
+const InnerDatepicker: React.SFC<any> = ({ date, fromDate, toDate, ...props }) => {
+  const [singleDate, setSingleDate] = useManagedState(parseDateString(date))
+  const [rangeDate, setRangeDate] = useManagedState([parseDateString(fromDate), parseDateString(toDate)])
+  const onChange = React.useCallback(({ date }) => {
+    if (props.range) {
+      setRangeDate(date)
+    } else {
+      setSingleDate(date)
+    }
+  }, [])
+
+  return <System.Datepicker {...props} value={props.range ? rangeDate : singleDate} onChange={onChange} />
 }
 
 export const Datepicker = withHOC(InnerDatepicker)
 
-const defaultFormat = "yyyy-mm-dd"
+const defaultFormat = "yyyy-MM-dd"
 Datepicker.defaultProps = {
   width: 150,
   height: 50,
@@ -33,4 +50,27 @@ addPropertyControls(Datepicker, {
     defaultValue: defaultFormat,
   }),
   formatString: merge(controls.formatString, { defaultValue: defaultFormat }),
+  date: {
+    type: ControlType.String,
+    title: "Date",
+    defaultValue: new Date().toISOString(),
+    hidden: props => props.range,
+  },
+  fromDate: {
+    type: ControlType.String,
+    title: "From Date",
+    defaultValue: new Date().toISOString(),
+    hidden: props => !props.range,
+  },
+  toDate: {
+    type: ControlType.String,
+    title: "To Date",
+    defaultValue: new Date().toISOString(),
+    hidden: props => !props.range,
+  },
+  range: {
+    type: ControlType.Boolean,
+    title: "Range",
+    defaultValue: false,
+  },
 })
