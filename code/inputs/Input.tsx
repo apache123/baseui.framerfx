b4abfe82255f340ebc8896d255f6b68558ee451e
Tisho Georgiev <t@tisho.co>
2019-10-11 08:04:46 -0700

    Removed BaseInput

--- code/inputs/Input.tsx
+++ code/inputs/Input.tsx
@@ -1,35 +1,41 @@
-import * as System from "baseui/input"
-import { addPropertyControls, ControlType } from "framer"
-import * as React from "react"
-import { compose } from "../utils/compose"
-import { controls, merge } from "../generated/Input"
-import { UseGlobalStatePropertyControls } from "../utils/PropertyControls"
-import { withManagedState } from "../utils/stateManagement/withManagedState"
-import { withHOC } from "../withHOC"
+import * as System from 'baseui/input';
+import {addPropertyControls, ControlType} from 'framer';
+import * as React from 'react';
+import {compose} from '../utils/compose';
+import {controls, merge} from '../generated/Input';
+import {UseGlobalStatePropertyControls} from '../utils/PropertyControls';
+import {withManagedState} from '../utils/stateManagement/withManagedState';
+import {withHOC} from '../withHOC';
 
-const InnerInput: React.SFC<any> = ({ onChange, ...props }) => {
-  const onValueChange = React.useCallback(e => onChange(e.target.value), [onChange])
-  return <System.Input onChange={onValueChange} {...props} />
-}
+const InnerInput: React.SFC<any> = ({onChange, ...props}) => {
+  const onValueChange = React.useCallback(e => onChange(e.target.value), [onChange]);
+  return <System.Input onChange={onValueChange} {...props} />;
+};
 
 export const Input = compose(
   withHOC,
-  withManagedState
-)(InnerInput)
+  withManagedState,
+)(InnerInput);
 
 Input.defaultProps = {
   width: 150,
   height: 50,
-  valuePropName: "value",
-}
+  valuePropName: 'value',
+};
 
 addPropertyControls(Input, {
-  clearable: merge(controls.clearable, { defaultValue: true }),
+  clearable: merge(controls.clearable, {defaultValue: true}),
   disabled: merge(controls.disabled, {}),
   error: merge(controls.error, {}),
   positive: merge(controls.positive, {}),
-  placeholder: merge(controls.placeholder, { defaultValue: "placeholder" }),
+  placeholder: merge(controls.placeholder, {defaultValue: 'Placeholder'}),
   value: merge(controls.value, {}),
-  type: { type: ControlType.Enum, options: ["text", "password", "number"], defaultValue: "text" },
+  type: {
+    type: ControlType.Enum,
+    options: ['text', 'password', 'number'],
+    optionTitles: ['Text', 'Password', 'Number'],
+    defaultValue: 'text',
+  },
+  size: merge(controls.size, {}),
   ...UseGlobalStatePropertyControls,
-})
+});
--- code/inputs/Textarea.tsx
+++ code/inputs/Textarea.tsx
@@ -1,29 +1,32 @@
-import * as System from "baseui/textarea"
-import { addPropertyControls } from "framer"
-import * as React from "react"
-import { controls, merge } from "../generated/Textarea"
-import { withHOC } from "../withHOC"
-import { useManagedState } from "../utils/useManagedState"
+import * as System from 'baseui/textarea';
+import {addPropertyControls} from 'framer';
+import * as React from 'react';
+import {controls, merge} from '../generated/Textarea';
+import {withHOC} from '../withHOC';
+import {useManagedState} from '../utils/useManagedState';
 
-const InnerTextarea: React.SFC<any> = ({ value, onChange: originalOnChange, ...props }) => {
-  const [currentValue, setValue] = useManagedState(value)
+const InnerTextarea: React.SFC<any> = ({value, onChange: originalOnChange, ...props}) => {
+  const [currentValue, setValue] = useManagedState(value);
 
-  const onChange = React.useCallback(e => {
-    setValue(e.target["value"])
-    if (typeof originalOnChange === 'function') {
-      originalOnChange(e.target["value"])
-    }
-  }, [originalOnChange])
+  const onChange = React.useCallback(
+    e => {
+      setValue(e.target['value']);
+      if (typeof originalOnChange === 'function') {
+        originalOnChange(e.target['value']);
+      }
+    },
+    [originalOnChange],
+  );
 
-  return <System.Textarea value={currentValue} onChange={onChange} {...props} />
-}
+  return <System.Textarea value={currentValue} onChange={onChange} {...props} />;
+};
 
-export const Textarea = withHOC(InnerTextarea)
+export const Textarea = withHOC(InnerTextarea);
 
 Textarea.defaultProps = {
   width: 300,
   height: 150,
-}
+};
 
 addPropertyControls(Textarea, {
   rows: merge(controls.rows, {
@@ -35,7 +38,7 @@ addPropertyControls(Textarea, {
   disabled: merge(controls.disabled, {}),
   error: merge(controls.error, {}),
   positive: merge(controls.positive, {}),
-  placeholder: merge(controls.placeholder, { defaultValue: "Placeholder!" }),
-  size: merge(controls.size, { defaultValue: "default" }),
-  value: merge(controls.value, { defaultValue: "" }),
-})
+  placeholder: merge(controls.placeholder, {defaultValue: 'Placeholder'}),
+  size: merge(controls.size, {defaultValue: 'default'}),
+  value: merge(controls.value, {defaultValue: ''}),
+});
